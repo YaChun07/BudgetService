78cf9b10fd2c97a4035a7b16d69850e88a3411f8
kyo.lai82 <kyo.lai82@titansoft.com.sg>
2018-12-08 22:44:05 +0800

    move period to single file

--- BudgetServiceTdd/BudgetService.cs
+++ BudgetServiceTdd/BudgetService.cs
@@ -4,18 +4,6 @@ using System.Linq;
 
 namespace BudgetServiceTdd
 {
-	public class Period
-	{
-		public Period(DateTime start, DateTime end)
-		{
-			Start = start;
-			End = end;
-		}
-
-		public DateTime Start { get; private set; }
-		public DateTime End { get; private set; }
-	}
-
 	public class BudgetService
 	{
 		private readonly List<Budget> _budgets;
